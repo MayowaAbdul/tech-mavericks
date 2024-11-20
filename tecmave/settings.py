096ae195a0868270a918d710695973281ed7440b
josh-adex1 <adewunmijoshua138@gmail.com>
2024-11-20 03:18:13 +0100

    updated

--- tecmave/settings.py
+++ tecmave/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY: str = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tech-mavericks.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
